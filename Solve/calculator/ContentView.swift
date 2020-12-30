b52116944254f4d9af0f2b24c8b677cdca13cf22
aishathaher <aisha.thaher@arch.ku.edu.kw>
2020-12-30 16:27:25 +0300

    Done

---
+++ Solve/calculator/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  calculator
+//
+//  Created by عايشة on 25/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+      
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Solve/calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by عايشة on 25/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
